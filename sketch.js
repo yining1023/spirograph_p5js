155d8210d587230f7a46780c18cfd43afdae4370
Yining Shi <ys2189@nyu.edu>
2017-12-29 17:18:06 -0500

    added color

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ let m = 3, n = 3;
 function setup() { 
   createCanvas(windowWidth, windowHeight);
   background(220);
+  colorMode(HSB, 100);
   noFill();
   for (let j = 0; j < n; j++) {
     for (let i = 0; i < m; i++) {
@@ -24,9 +25,9 @@ function Graph(centerX, centerY) {
   this.centerX = centerX;
   this.centerY = centerY;
   this.ang = 0;
-  this.R = Math.floor(random(80, 120));
+  this.R = Math.floor(random(90, 120));
   this.r = Math.floor(random(20, this.R - 20));
-  this.p = Math.floor(random(5, this.r - 5));
+  this.p = Math.floor(random(10, this.r - 10));
   this.k = this.r / this.R;
   this.l = this.p / this.r;
 
@@ -39,10 +40,12 @@ function Graph(centerX, centerY) {
   
       this.x = this.R * ((1 - this.k) * Math.cos(t) + ((this.l * this.k) * Math.cos(this.ang)));
       this.y = this.R * ((1 - this.k) * Math.sin(t) - ((this.l * this.k) * Math.sin(this.ang)));
-  
+      
+      let h = Math.floor((((step / 150 + this.centerX))) % 100);
+      stroke(h, 60, 80);
       line(this.prevX, this.prevY, this.x, this.y,);
       pop();
-      step += 2;
+      step += 1;
       this.prevX = this.x;
       this.prevY = this.y;
     }
