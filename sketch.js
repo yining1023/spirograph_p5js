2d37d5309a363cf299bebdc1b5f181620a14dee1
Yining Shi <ys2189@nyu.edu>
2017-12-31 14:38:25 -0500

    refactor and clean up

--- sketch.js
+++ sketch.js
@@ -1,11 +1,9 @@
-let step = 0;
-let t;
 let graphs = [];
-let m = 3, n = 3;
+const m = 3, n = 3;
 
 function setup() { 
   createCanvas(windowWidth, windowHeight);
-  background(220);
+  background(255);
   colorMode(HSB, 100);
   noFill();
   for (let j = 0; j < n; j++) {
@@ -21,31 +19,33 @@ function draw() {
   });
 }
 
-function Graph(centerX, centerY) {
-  this.centerX = centerX;
-  this.centerY = centerY;
-  this.ang = 0;
-  this.R = Math.floor(random(90, 120));
-  this.r = Math.floor(random(20, this.R - 20));
-  this.p = Math.floor(random(10, this.r - 10));
-  this.k = this.r / this.R;
-  this.l = this.p / this.r;
+class Graph {
+  constructor(centerX, centerY) {
+    this.centerX = centerX;
+    this.centerY = centerY;
+    this.step = 0;
+    this.R = Math.floor(random(90, 120));
+    this.r = Math.floor(random(20, this.R - 20));
+    this.p = Math.floor(random(10, this.r - 10));
+  }
 
-  this.display = function() {
-    if (step < 30000) {  
+  display() {
+    if (this.step < 40000) {
       push();
       translate(this.centerX, this.centerY);
-      t = radians(step);
-      this.ang = ((this.l - this.k) / this.k) * t;
+      let t = radians(this.step);
+      let k = this.r / this.R;
+      let l = this.p / this.r;
+      let ang = ((l - k) / k) * t;
   
-      this.x = this.R * ((1 - this.k) * Math.cos(t) + ((this.l * this.k) * Math.cos(this.ang)));
-      this.y = this.R * ((1 - this.k) * Math.sin(t) - ((this.l * this.k) * Math.sin(this.ang)));
+      this.x = this.R * ((1 - k) * Math.cos(t) + ((l * k) * Math.cos(ang)));
+      this.y = this.R * ((1 - k) * Math.sin(t) - ((l * k) * Math.sin(ang)));
       
-      let h = Math.floor((((step / 150 + this.centerX))) % 100);
+      let h = Math.floor((((this.step / 150 + this.centerX))) % 100);
       stroke(h, 60, 80);
-      line(this.prevX, this.prevY, this.x, this.y,);
+      line(this.prevX, this.prevY, this.x, this.y);
       pop();
-      step += 1;
+      this.step += 8;
       this.prevX = this.x;
       this.prevY = this.y;
     }
